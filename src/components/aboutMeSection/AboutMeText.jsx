0063519ab5c2c8409111f33a44718c6d712aa9ee
karin goldin <karingoldin@Mac.lan>
2025-07-28 16:50:16 +0300

    Update: Skills and About Me section content

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -8,11 +8,11 @@ const AboutMeText = () => {
       <p className="text-white">
         Hello there! I'm a front-end developer with 2 years of experience,
         specializing in ReactJS. I have a deep passion for creating interactive
-        and dynamic user interfaces that provide seamless user experiences.
-        <br /> My love for coding began during my studies and since then, I've
-        been committed to continuously learning and growing my skill set. <br />{" "}
-        I'm constantly on the lookout for new challenges and opportunities to
-        apply my expertise in developing web applications.
+        and dynamic user interfaces that provide seamless user experiences.{" "}
+        <br /> My love for coding began during my studies, and since then I've
+        been committed to continuously learning and improving my skill set. I'm
+        always on the lookout for new challenges and opportunities to apply my
+        expertise in developing web applications.
       </p>
       <button className="border border-orange rounded-full py-2 px-4 text-lg flex items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-cyan">
         <Link
